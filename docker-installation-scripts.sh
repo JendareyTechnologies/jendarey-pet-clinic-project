b080b928dc076eda0be7976290b6129cea079c53
JendareyTechnologies <ak@jendareyconsulting.com>
2024-06-09 20:17:24 -0500

    Update docker-installation-scripts.sh

--- docker-installation-scripts.sh
+++ docker-installation-scripts.sh
@@ -5,25 +5,24 @@ sudo apt update
 sudo hostnamectl set-hostname jendarey-docker-engine
 
 # Install Dependencies (only if not already installed)
-sudo apt install -y git curl maven tree
+sudo apt install -y git curl maven tree openjdk-17-jre-headless
 
-# Set up Docker's apt repository.
 # Add Docker's official GPG key:
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 
-#Install the latest version, run:
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+# Install the Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Add the current user to the 'docker' group
 sudo usermod -aG docker ubuntu
